40846bf3e7414a9002af3b89ffcf28833a70beb5
alikaliusman1 <alikaliusman1@gmail.com>
2023-12-18 10:21:08 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A ptogram that print a line using the printf function
+ * Return: 0
+ */
+int main(void)
+{
+	Printf("with proper grammar, but the outcome is a piece of art,\n")
+      return (0);
+}
